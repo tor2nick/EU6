6f95d6f67860426311a1567b19808c3bcda2dbb2
tor2nick <tor2nick@yahoo.com>
2022-11-28 09:38:12 +0200

    EU6_TestNG

---
+++ target/failed-html-report/report.js
@@ -0,0 +1,198 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/Expenses.feature");
+formatter.feature({
+  "name": "Расход",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@wip"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Расход",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@wip"
+    }
+  ]
+});
+formatter.step({
+  "name": "Пользователь переходит на страницу входа",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Пользователь входит в систему с действительными учетными данными администратора на второй странице входа.",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Пользователь кликает \u0027Добавить платеж\u0027",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Пользователь выбирает \u0027Расход\u0027 radio button",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Пользователь выбирает \u0027Уменьшаемый план\u0027 from dropdown",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "\u0027Сумма факт\u0027, \u0027Счет отправителя\u0027 and \u0027Счет получателя\u0027 is disappeared",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "\u0027Когда\u0027 block is disappeared",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Related Payments appeared",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.uri("file:src/test/resources/features/Login.feature");
+formatter.feature({
+  "name": "Login",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@wip"
+    },
+    {
+      "name": "@login"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Login",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@wip"
+    },
+    {
+      "name": "@login"
+    }
+  ]
+});
+formatter.step({
+  "name": "Пользователь переходит на страницу входа",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Пользователь входит в систему с действительными учетными данными администратора на второй странице входа.",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Пользователь попадает на главную страницу",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.uri("file:src/test/resources/features/Payments.feature");
+formatter.feature({
+  "name": "Новый платеж",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@wip"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Новый платёж",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@wip"
+    }
+  ]
+});
+formatter.step({
+  "name": "Пользователь переходит на страницу входа",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Пользователь входит в систему с действительными учетными данными администратора на второй странице входа.",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Пользователь попадает на главную страницу",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Пользователь кликает \u0027Добавить платеж\u0027",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Пользователь попадает на \u0027Добавить платеж\u0027 page",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
